c4d00409a68efa68feaa2483a8bb0e7740e6517b
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2021-08-22 23:49:01 +0900

    add cmake.init task

--- lib/mix/tasks/cmake.ex
+++ lib/mix/tasks/cmake.ex
@@ -1,6 +1,52 @@
 defmodule Mix.Tasks.Cmake do
   alias Mix.Tasks.Cmake
-  
+
+  defmodule Init do
+    use Mix.Task
+    import Mix.Generator
+    
+    def run(_argv) do
+      assigns = [
+        app_name: Cmake.app_name()
+      ]
+    
+      create_file("CMakeLists.txt", cmakelists_template(assigns))
+    end
+    
+    embed_template(:cmakelists, """
+    #
+    # Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #      https://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+    
+    cmake_minimum_required(VERSION 3.18)
+    project(<%= @app_name %> CXX)
+    
+    
+    # main
+    add_executable(<%= @app_name %>
+      src/<%= @app_name %>.cc
+    )
+    target_link_libraries(<%= @app_name %>
+    )
+    
+    install(TARGETS <%= @app_name %>
+      RUNTIME
+      DESTINATION ${CMAKE_SOURCE_DIR}/priv
+    )
+    """)
+  end
+
   defmodule Config do
     use Mix.Task
 
@@ -98,7 +144,8 @@ defmodule Mix.Tasks.Cmake do
     cmake(build_path, args ++ [source_path], env)
   end
 
-  def build(build_dir, args \\ [], env \\ %{}),   do: cmake(build_path(build_dir), ["--build", "."] ++ args, env)
+  def build(build_dir, args \\ [], env \\ %{}), do: cmake(build_path(build_dir), ["--build", "."] ++ args, env)
+
   def install(build_dir, args \\ [], env \\ %{}), do: cmake(build_path(build_dir), ["--install", "."] ++ args, env)
 
   defp cmake(build_path, args, env) do
@@ -115,15 +162,18 @@ defmodule Mix.Tasks.Cmake do
     (status == 0)
   end
 
+  def app_name() do
+    Mix.Project.config[:app]
+    |> Atom.to_string()
+  end
+
   defp build_path(:local) do
     Mix.Project.build_path()
     |> Path.join("cmake")
   end
 
   defp build_path(:global) do
-    app_name =
-      Mix.Project.config[:app]
-      |> Atom.to_string()
+    app_name = app_name()
 
     System.user_home
     |> Path.absname()
